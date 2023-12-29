9b7b9de12a120d4f27cbe9e52e93ce39278706d2
XNoahx777 <95333287+XNoahx777@users.noreply.github.com>
2023-12-29 13:43:14 -0800

    Update Logo.jsx

--- src/components/others/Logo.jsx
+++ src/components/others/Logo.jsx
@@ -1,16 +1,9 @@
 export default function Logo({ width = 100 }) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      xmlnsXlink="http://www.w3.org/1999/xlink"
-      width={width}
-      viewBox="0 0 100 30" // Ajusta el viewBox según el tamaño de tu logo
-    >
-      <text x="0" y="20" fill="black">ELA Store</text>
-    </svg>
+    <div>
+      ELA store
+    </div>
   );
 }
 
-    </svg>
-  );
 }
